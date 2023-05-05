bebe98cc78bceb03824f54ea96f325a015a64aec
tunaabop <tinawang158@gmail.com>
2023-05-04 23:11:47 -0700

    added install and db script content

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,28 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    // store triggered events
+    window.deferredPrompt = event;
+    // get rid of hidden class from our button
+    butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+     return;
+    }
+    promptEvent.prompt();
+    // reset deferred prompt var for one-time use
+    window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    // this clears prompt
+    window.deferredPrompt = null;
+});
